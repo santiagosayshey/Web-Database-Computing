e54857a0e241bbf5063f50636129ee09321e00c6
santiagosayshey <chaucted@gmail.com>
2023-03-14 17:08:58 +0000

    Mar 14, 2023, 17:08:58 UTC

--- task3.js
+++ task3.js
@@ -1,7 +1,8 @@
 let mCounter = 0;
+
 function mcount() {
-  var parent = document.getElementById("mcount");
   var child = document.createElement("P");
+  var parent = document.getElementById("mcount");
   if (mCounter === 0) {
     parent.appendChild(child);
   }
