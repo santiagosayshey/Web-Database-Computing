d3f4905a9a045e2dbe97f7d1c9a948dbb06e3c11
santiagosayshey <chaucted@gmail.com>
2023-03-14 17:21:07 +0000

    Mar 14, 2023, 17:21:07 UTC

--- task3.js
+++ task3.js
@@ -4,3 +4,10 @@ function counter() {
   var div = document.getElementById("mcount");
   div.innerHTML = count;
 }
+
+function post() {
+  var time = new Date();
+  var temp = document.getElementsByTagName("textarea");
+  var content = temp[0].value;
+  
+}
