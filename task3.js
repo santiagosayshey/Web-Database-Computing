a746b12a221165e633816d1ffa335aa19644cd60
santiagosayshey <chaucted@gmail.com>
2023-03-14 17:50:08 +0000

    Mar 14, 2023, 17:50:08 UTC

--- task3.js
+++ task3.js
@@ -4,20 +4,23 @@ function counter(parent) {
   parent.innerHTML = count;
 }
 
+
+
 function post() {
   var time = new Date();
   var temp = document.getElementsByTagName("textarea");
   var content = temp[0].value;
 
   var post_time = document.createElement('p');
+  var post_content = document.createElement('p');
+  var parent = document.getElementById('posts');
+
   post_time.className = "post_time";
   post_time.innerText = time;
 
-  var post_content = document.createElement('p');
   post_content.className = "post_content";
   post_content.innerText = content;
 
-  var parent = document.getElementById('posts');
   parent.appendChild(post_time);
   parent.appendChild(post_content);
 
@@ -39,7 +42,6 @@ function hideMenu() {
   menu.style.display = "none";
 }
 
-function postColor(color)
-{
-  
+function postColor(color) {
+
 }
